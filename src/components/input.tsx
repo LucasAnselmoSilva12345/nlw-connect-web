d52df60bedb64d5b07e12ca6d1c6099cc84bed74
LucasAnselmoSilva12345 <lucasanselmodasilva02@gmail.com>
2025-02-17 22:07:17 -0300

    feat: BiomeJS library
    Signed-off-by: LucasAnselmoSilva12345 <lucasanselmodasilva02@gmail.com>

--- src/components/input.tsx
+++ src/components/input.tsx
@@ -1,7 +1,7 @@
-import { ComponentProps } from 'react';
+import type { ComponentProps } from 'react'
 
 interface InputRootProps extends ComponentProps<'div'> {
-  error?: boolean;
+  error?: boolean
 }
 
 export function InputRoot({ error = false, ...props }: InputRootProps) {
@@ -11,7 +11,7 @@ export function InputRoot({ error = false, ...props }: InputRootProps) {
       className="group bg-gray-800 h-12 border border-gray-600 rounded-xl px-4 flex items-center gap-2 focus-within:border-gray-100 data-[error=true]:border-danger"
       {...props}
     />
-  );
+  )
 }
 
 interface InputIconProps extends ComponentProps<'span'> {}
@@ -22,7 +22,7 @@ export function InputIcon(props: InputIconProps) {
       className="text-gray-400 group-focus-within:text-gray-100 group-[&:not(:has(input:placeholder-shown))]:text-gray-100 group-data-[error=true]:text-danger"
       {...props}
     />
-  );
+  )
 }
 
 interface InputFieldProps extends ComponentProps<'input'> {}
@@ -30,5 +30,5 @@ interface InputFieldProps extends ComponentProps<'input'> {}
 export function InputField(props: InputFieldProps) {
   return (
     <input className="flex-1 outline-0 placeholder:text-gray-400" {...props} />
-  );
+  )
 }

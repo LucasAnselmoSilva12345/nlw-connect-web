af75fc844fd9662e1ca7693b67043d81083b525c
LucasAnselmoSilva12345 <lucasanselmodasilva02@gmail.com>
2025-02-17 22:10:47 -0300

    feat: Assets Image
    Signed-off-by: LucasAnselmoSilva12345 <lucasanselmodasilva02@gmail.com>

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,31 +1,8 @@
-import { Button } from '@/components/button';
-import { IconButton } from '@/components/icon-button';
-import { InputField, InputIcon, InputRoot } from '@/components/input';
-import { ArrowRight, Copy, Mail } from 'lucide-react';
+import { Button } from '@/components/button'
+import { IconButton } from '@/components/icon-button'
+import { InputField, InputIcon, InputRoot } from '@/components/input'
+import { ArrowRight, Copy, Mail } from 'lucide-react'
 
 export default function Home() {
-  return (
-    <main>
-      <h1 className="text-3xl font-bold underline">Hello world!</h1>
-
-      <Button>
-        Enviar
-        <ArrowRight />
-      </Button>
-
-      <IconButton>
-        <Copy />
-      </IconButton>
-
-      <div>
-        <InputRoot>
-          <InputIcon>
-            <Mail className="size-5" />
-          </InputIcon>
-
-          <InputField />
-        </InputRoot>
-      </div>
-    </main>
-  );
+  return <main>Main</main>
 }

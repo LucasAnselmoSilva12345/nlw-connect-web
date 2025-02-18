2e05488a06b7021fce379e61f8ab5c0a994cc385
LucasAnselmoSilva12345 <lucasanselmodasilva02@gmail.com>
2025-02-17 21:45:48 -0300

    feat: Input component
    Signed-off-by: LucasAnselmoSilva12345 <lucasanselmodasilva02@gmail.com>

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,6 @@
 import { Button } from '@/components/button';
 import { IconButton } from '@/components/icon-button';
+import { Input } from '@/components/input';
 import { ArrowRight, Copy } from 'lucide-react';
 
 export default function Home() {
@@ -15,6 +16,11 @@ export default function Home() {
       <IconButton>
         <Copy />
       </IconButton>
+
+      <div>
+        <Input type="text" placeholder="Digite seu e-mail" />
+        <Input type="text" placeholder="Digite seu e-mail" error />
+      </div>
     </main>
   );
 }

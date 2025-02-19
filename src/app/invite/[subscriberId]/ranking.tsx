1fd711bbb57db0b171a9cc4615fd2577c27b138a
LucasAnselmoSilva12345 <lucasanselmodasilva02@gmail.com>
2025-02-19 20:53:31 -0300

    feat: Set Ranking data
    Signed-off-by: LucasAnselmoSilva12345 <lucasanselmodasilva02@gmail.com>

--- src/app/invite/[subscriberId]/ranking.tsx
+++ src/app/invite/[subscriberId]/ranking.tsx
@@ -1,57 +1,58 @@
+import { getRanking } from '@/http/api'
 import Image from 'next/image'
 import medalCooper from '../../../assets/medal-cooper.svg'
 import medalGold from '../../../assets/medal-gold.svg'
 import medalSilver from '../../../assets/medal-silver.svg'
 
-export function Ranking() {
+export async function Ranking() {
+  const { ranking } = await getRanking()
+
   return (
     <div className="w-full max-w-[440px] space-y-5">
       <h2 className="text-gray-200 text-xl font-heading font-semibold leading-none">
         Ranking de indicações
       </h2>
       <div className="space-y-4">
-        <div className="relative rounded-xl bg-gray-700 border border-gray-600 p-6 flex flex-col justify-center gap-3">
-          <span className="text-sm text-gray-300 leading-none">
-            <span className="font-semibold">1°</span> | Lucas Anselmo
-          </span>
-          <span className="font-heading text-2xl font-semibold text-gray-200 leading-none">
-            1022
-          </span>
+        {ranking.map((item, index) => {
+          const rankingPosition = index + 1
 
-          <Image
-            src={medalGold}
-            alt="Medal Gold"
-            className="absolute top-0 right-8"
-          />
-        </div>
-        <div className="relative rounded-xl bg-gray-700 border border-gray-600 p-6 flex flex-col justify-center gap-3">
-          <span className="text-sm text-gray-300 leading-none">
-            <span className="font-semibold">2°</span> | Diego Fernandes
-          </span>
-          <span className="font-heading text-2xl font-semibold text-gray-200 leading-none">
-            1000
-          </span>
+          return (
+            <div
+              key={item.id}
+              className="relative rounded-xl bg-gray-700 border border-gray-600 p-6 flex flex-col justify-center gap-3"
+            >
+              <span className="text-sm text-gray-300 leading-none">
+                <span className="font-semibold">{rankingPosition}°</span> |{' '}
+                {item.name}
+              </span>
+              <span className="font-heading text-2xl font-semibold text-gray-200 leading-none">
+                {item.score}
+              </span>
 
-          <Image
-            src={medalSilver}
-            alt="Medal Gold"
-            className="absolute top-0 right-8"
-          />
-        </div>
-        <div className="relative rounded-xl bg-gray-700 border border-gray-600 p-6 flex flex-col justify-center gap-3">
-          <span className="text-sm text-gray-300 leading-none">
-            <span className="font-semibold">3°</span> | Mayk Brito
-          </span>
-          <span className="font-heading text-2xl font-semibold text-gray-200 leading-none">
-            980
-          </span>
-
-          <Image
-            src={medalCooper}
-            alt="Medal Gold"
-            className="absolute top-0 right-8"
-          />
-        </div>
+              {rankingPosition === 1 && (
+                <Image
+                  src={medalGold}
+                  alt="Medal Gold"
+                  className="absolute top-0 right-8"
+                />
+              )}
+              {rankingPosition === 2 && (
+                <Image
+                  src={medalSilver}
+                  alt="Medal Gold"
+                  className="absolute top-0 right-8"
+                />
+              )}
+              {rankingPosition === 3 && (
+                <Image
+                  src={medalCooper}
+                  alt="Medal Gold"
+                  className="absolute top-0 right-8"
+                />
+              )}
+            </div>
+          )
+        })}
       </div>
     </div>
   )

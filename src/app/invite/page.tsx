655ab0c66182a5d994c164655863c84160d60731
LucasAnselmoSilva12345 <lucasanselmodasilva02@gmail.com>
2025-02-18 22:36:50 -0300

    feat: Invite Link Input Copy clipboard text
    Signed-off-by: LucasAnselmoSilva12345 <lucasanselmodasilva02@gmail.com>

--- src/app/invite/page.tsx
+++ src/app/invite/page.tsx
@@ -4,6 +4,8 @@ import { Ranking } from './ranking'
 import { Stats } from './stats'
 
 export default function InvitePage() {
+  const inviteLink = 'http://localhost:3000/codecraft-summit-2025/12852'
+
   return (
     <div className="min-h-dvh flex items-center justify-between gap-16 flex-col md:flex-row">
       <div className="flex flex-col gap-10 w-full max-w-[550px]">
@@ -29,7 +31,7 @@ export default function InvitePage() {
               inscrições:
             </p>
           </div>
-          <InviteLinkInput />
+          <InviteLinkInput inviteLink={inviteLink} />
 
           <Stats />
         </div>
